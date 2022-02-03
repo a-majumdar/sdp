d1ea676eb6be3d626b2dfcbda82e19e826eb156e
s1937645 <60388285+s1937645@users.noreply.github.com>
2022-02-03 14:11:27 +0000

    EricUpdate

--- software/src/App.js
+++ software/src/App.js
@@ -4,6 +4,9 @@ import Navbar from "./components/organisms/Navbar";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./screens/Home";
 import SignUp from "./screens/SignUp";
+import AboutUs from "./screens/AboutUs";
+import Contact from "./screens/ContactUs";
+import Explore from "./screens/Explore";
 
 function App() {
   return (
@@ -13,6 +16,9 @@ function App() {
         <Switch>
           <Route path="/" exact component={Home} />
           <Route path="/sign-up" exact component={SignUp} />
+          <Route path="/about-us" exact component={AboutUs} />
+          <Route path="/Contact" exact component={Contact} />
+          <Route path="/Explore" exact component={Explore} />
         </Switch>
       </Router>
     </>
---
+++ software/src/components/organisms/auHeroSection.js
@@ -0,0 +1,23 @@
+import React from "react";
+import PlantBackground from "../../assets/ePlant.jpg";
+import "../organisms/auHeroSection.css";
+import "../../App";
+import { Button } from "../atoms/Button";
+
+function HeroSection() {
+  return (
+    <div className="hero-container">
+      <img className="background-image" src={PlantBackground}></img>
+      <h1> We are PlantEd</h1>
+      {/* <p>
+        Learn, Explore and Interact with your plants and give them the best
+        start in life.
+      </p> */}
+      <div className="hero-btns">
+        
+      </div>
+    </div>
+  );
+}
+
+export default HeroSection;
