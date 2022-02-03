cfdb57d28cbe0e675bb2c37afa306a69e4f5bc46
s1937645 <60388285+s1937645@users.noreply.github.com>
2022-02-03 15:04:11 +0000

    Contact&AboutUs

--- software/src/components/organisms/auHeroSection.js
+++ software/src/components/organisms/auHeroSection.js
@@ -5,14 +5,22 @@ import "../../App";
 import { Button } from "../atoms/Button";
 
 function HeroSection() {
+    const mystyle = {
+    color: "white",
+    textAlign: 'center',
+    textAlignVertical: "center",
+    fontSize: "45px",
+    padding: "10px",
+  };
   return (
     <div className="hero-container">
       <img className="background-image" src={PlantBackground}></img>
       <h1> We are PlantEd</h1>
-      {/* <p>
-        Learn, Explore and Interact with your plants and give them the best
-        start in life.
-      </p> */}
+      <p style={mystyle}>
+      PlantEd is a smart plant care system that intends to enhance the 
+      learning curve of new plant enthusiasts by providing an interactive 
+      experience for growing plants indoors, and by assessing the optimal conditions for growth.
+      </p>
       <div className="hero-btns">
         
       </div>
--- software/src/components/organisms/cuHeroSection.js
+++ software/src/components/organisms/cuHeroSection.js
@@ -5,14 +5,29 @@ import "../../App";
 import { Button } from "../atoms/Button";
 
 function HeroSection() {
+  const mystyle = {
+    color: "white",
+    textAlign: 'center',
+    textAlignVertical: "center",
+    fontSize: "30px",
+    padding: "10px",
+  };
   return (
     <div className="hero-container">
       <img className="background-image" src={PlantBackground}></img>
-      
-      {/* <p>
-        Learn, Explore and Interact with your plants and give them the best
-        start in life.
-      </p> */}
+      <h1> Get in Touch</h1>
+      <p style={mystyle}>
+      Here at PlantEd we love hearing your feedback and questions just email one us below:
+      </p>
+      <p style={mystyle}>Ananya Majumdar (Project Manager) - s1802817@ed.ac.uk</p>
+      <p style={mystyle}>Oscar Alberigo (Software Developer) - s1861004@ed.ac.uk</p>
+      <p style={mystyle}>Fadi Barazi (Hardware Developer) - s1969801@ed.ac.uk</p>
+      <p style={mystyle}>Dafydd Evans Foster (Hardware Developer) - s1934523@ed.ac.uk</p>
+      <p style={mystyle}>Eric Gallagher (Software Developer) - s1937645@ed.ac.uk@ed.ac.uk</p>
+      <p style={mystyle}>Casey Gong (Software Developer) - s1912801@ed.ac.uk</p>
+      <p style={mystyle}>Susanna Lassila (Software Developer) - s1912801@ed.ac.uk</p>
+      <p style={mystyle}>Binbin Zhan (Software Developer) - s1945599@ed.ac.uk</p>
+      <p style={mystyle}>Bohan Xu (Hardware Developer) - s1931736@ed.ac.uk</p>
       <div className="hero-btns">
         
       </div>
