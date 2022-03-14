59473f3b9622b9ec1d7c9c1f825e3cdc1d025d7f
s1937645 <60388285+s1937645@users.noreply.github.com>
2022-03-14 12:27:00 +0000

    annoying css done

--- software/src/screens/PropagatorPage.js
+++ software/src/screens/PropagatorPage.js
@@ -2,6 +2,10 @@ import React, { useState, useContext, useEffect } from "react";
 import { Slider } from '@material-ui/core'
 import { Card, Form, Button, Alert, Container } from "react-bootstrap";
 import PlantBackground from "../assets/back.jpg";
+import sun from "../assets/sun.png";
+import tempic from "../assets/thermom.png";
+import humid from "../assets/humid.png";
+import moist from "../assets/moist.png";
 import "../screens/PropagatorPage.css";
 import Footer from "../components/organisms/Footer";
 import plant from "../assets/greenplant.jpg";
@@ -32,6 +36,7 @@ const muiTheme = createMuiTheme({
       thumb:{
       color: "#3399ff",
       },
+      
       track: {
         color: 'red'
       },
@@ -45,6 +50,12 @@ const muiTheme = createMuiTheme({
  * Propogator Section of Our Website
  * @returns
  */
+
+ const data = [
+  { name: "Anom", age: 19, gender: "Male" },
+  { name: "Megha", age: 19, gender: "Female" },
+  { name: "Subham", age: 25, gender: "Male"},
+]
 export default function MyProp() {
   const { currentUserUID, currentUserEmail } = useContext(AuthContext);
   const {
@@ -88,6 +99,7 @@ export default function MyProp() {
   const wtoggler = () => {
     toggle ? setwToggle(false) : setwToggle(true);
   }
+ 
 
   
   return (
@@ -95,64 +107,36 @@ export default function MyProp() {
       {/* 
       <img className="background-image" src={PlantBackground}></img>
       */}
-      <img class="propPageTitle" src={yourProp} alt="Logo" />
-      <br></br>
-      <h1>Welcome, {currentUserEmail}</h1>
+    <h1>Here's how your {plantCommonName} plant is doing: </h1>
+    <div class="tableAndVars">
+    <div className="outTable">
+      <table>
+        <tr>
+          <th>{<img class="sun" src={sun} alt="Logo" />}</th>
+          <th>Sunlight</th>
+          <th>{sunlight}</th>
+        </tr>
+        <tr>
+          <td>{<img class="sun" src={tempic} alt="Logo" />}</td>
+          <td>Temperature</td>
+          <td>{temperature}</td>
+        </tr>
+        <tr>
+          <td>{<img class="sun" src={humid} alt="Logo" />}</td>
+          <td>Humidity</td>
+          <td>{humidity}</td>
+        </tr>
+        <tr>
+          <td>{<img class="sun" src={moist} alt="Logo" />}</td>
+          <td>Moisture</td>
+          <td>{moisture}</td>
+        </tr>
+      </table>
+    </div>
       
-      <br></br>
-      
-      <div class="mycont">
-      <div class="divData">
-        <p class="one">
-          You are currently growing - {plantCommonName}
-          <br></br>
-          Temperature - {temperature} <br></br>
-          <button class="but1">-</button>
-          <button class="but3">+</button>
-          <br></br>
-          Sunlight - {sunlight}
-          <br></br>
-          <button class="but1">Low</button>
-          <button class="but3">Medium</button>
-          <button class="but3">High</button>
-          <br></br>
-          Humidity - {humidity}
-          <br></br>
-          <button class="but1">-</button>
-          <button class="but3">+</button>
-          <br></br>
-          Moisture - {moisture}
-          <br></br>
-          <button class="but1">Water</button>
-          <br></br>
-          Ventilation - {/* Whether vent is open or not  */}
-          <br></br>
-          <button className={mystyle} onClick={changeStyle}>
-            Open
-          </button>
-          <button className={mystyle2} onClick={changeStyle2}>
-            Closed
-          </button>
-          <br></br>
-        </p>
-        <button class="but4">Set Conditions Back to Default</button>
-        </div>
 
-
-
-      <div class="box">
-        <figure>
-        <figcaption class="mycaption">Propagator ID: {propId}</figcaption>
-          <figcaption class="mycaption">
-            ID of Plant Currently Growing: {plantIdWeb}
-          </figcaption>
-          <img class="picRight" src={plant} alt="Logo" />
-          
-        </figure>
-        </div>
         
-        </div>
-
+      <div class="menu">
       <div className="ventCont">
         <div className="vent">
           <h2>Vent</h2>
@@ -181,6 +165,7 @@ export default function MyProp() {
      
       </div>
 
+
       <div class ="slider2">
         <div class = "slider2text">
           <h2>Position: Tilt</h2>
@@ -203,24 +188,27 @@ export default function MyProp() {
           <Switch onClick={wtoggler}/>
         </div>
       </div>
-
-      {/*
-      <h1>Your plant,</h1>
-      <div className="container">
-      <img 
-      className='img'
-      src={plant} alt="Logo" 
-      />
       </div>
-       style={{width:'25%', height:"28"}}
-      <p>Tempertature</p>
-      {/* 
-      <p>Sunlight</p>
-      <p>Soil pH</p>
-      <p>Moisture</p>
-      <p>Humidity</p>
-      <p>Ventilation</p>
+      </div>
+      {/*
+
+      
+      
+      
       */}
+      {/* <div class="box">
+        <figure>
+        <figcaption class="mycaption">Propagator ID: {propId}</figcaption>
+          <figcaption class="mycaption">
+            ID of Plant Currently Growing: {plantIdWeb}
+          </figcaption>
+          
+          <img class="picRight" src={plant} alt="Logo" />
+      </figure>
+      </div> */}
+        
+
+  
 
       <Footer />
     </>
