30806a116663481263e961206df1c3e385770cdc
s1937645 <60388285+s1937645@users.noreply.github.com>
2022-03-30 16:16:54 +0100

    slider connected

--- software/node/app.js
+++ software/node/app.js
@@ -46,6 +46,237 @@ app.post("/closeVent", (req, res) => {
   res.redirect("/MyProp");
 });
 
+app.post("/moveProp", (req, res) => {
+  const SSH = require("simple-ssh");
+  const cmd = "eScript";
+  const thecmd = 'echo "!' + cmd + '.py" > file.txt';
+  //  console.log("inside the system " );
+  var ssh_options = new SSH({
+    host: "abomasnow",
+    user: "pi",
+    pass: "r00t",
+  });
+  //'echo "!eVentOpen.py" > file.txt'
+  ssh_options
+    .exec(thecmd, {
+      out: console.log.bind(console),
+    })
+    .start();
+
+  console.log("Executing command move 10" );
+  
+  res.redirect("/MyProp");
+});
+app.post("/moveProp20", (req, res) => {
+  const SSH = require("simple-ssh");
+  const cmd = "eScript";
+  const thecmd = 'echo "!' + cmd + '.py" > file.txt';
+  //  console.log("inside the system " );
+  var ssh_options = new SSH({
+    host: "abomasnow",
+    user: "pi",
+    pass: "r00t",
+  });
+  //'echo "!eVentOpen.py" > file.txt'
+  ssh_options
+    .exec(thecmd, {
+      out: console.log.bind(console),
+    })
+    .start();
+
+  console.log("Executing command move 20" );
+  
+  res.redirect("/MyProp");
+});
+app.post("/moveProp30", (req, res) => {
+  const SSH = require("simple-ssh");
+  const cmd = "eScript";
+  const thecmd = 'echo "!' + cmd + '.py" > file.txt';
+  //  console.log("inside the system " );
+  var ssh_options = new SSH({
+    host: "abomasnow",
+    user: "pi",
+    pass: "r00t",
+  });
+  //'echo "!eVentOpen.py" > file.txt'
+  ssh_options
+    .exec(thecmd, {
+      out: console.log.bind(console),
+    })
+    .start();
+
+  console.log("Executing command move 30" );
+  
+  res.redirect("/MyProp");
+});
+app.post("/moveProp40", (req, res) => {
+  const SSH = require("simple-ssh");
+  const cmd = "eScript";
+  const thecmd = 'echo "!' + cmd + '.py" > file.txt';
+  //  console.log("inside the system " );
+  var ssh_options = new SSH({
+    host: "abomasnow",
+    user: "pi",
+    pass: "r00t",
+  });
+  //'echo "!eVentOpen.py" > file.txt'
+  ssh_options
+    .exec(thecmd, {
+      out: console.log.bind(console),
+    })
+    .start();
+
+  console.log("Executing command move 40" );
+  
+  res.redirect("/MyProp");
+});
+app.post("/moveProp50", (req, res) => {
+  const SSH = require("simple-ssh");
+  const cmd = "eScript";
+  const thecmd = 'echo "!' + cmd + '.py" > file.txt';
+  //  console.log("inside the system " );
+  var ssh_options = new SSH({
+    host: "abomasnow",
+    user: "pi",
+    pass: "r00t",
+  });
+  //'echo "!eVentOpen.py" > file.txt'
+  ssh_options
+    .exec(thecmd, {
+      out: console.log.bind(console),
+    })
+    .start();
+
+  console.log("Executing command move 50" );
+  
+  res.redirect("/MyProp");
+});
+app.post("/moveProp60", (req, res) => {
+  const SSH = require("simple-ssh");
+  const cmd = "eScript";
+  const thecmd = 'echo "!' + cmd + '.py" > file.txt';
+  //  console.log("inside the system " );
+  var ssh_options = new SSH({
+    host: "abomasnow",
+    user: "pi",
+    pass: "r00t",
+  });
+  //'echo "!eVentOpen.py" > file.txt'
+  ssh_options
+    .exec(thecmd, {
+      out: console.log.bind(console),
+    })
+    .start();
+
+  console.log("Executing command move 60" );
+  
+  res.redirect("/MyProp");
+});
+app.post("/moveProp70", (req, res) => {
+  const SSH = require("simple-ssh");
+  const cmd = "eScript";
+  const thecmd = 'echo "!' + cmd + '.py" > file.txt';
+  //  console.log("inside the system " );
+  var ssh_options = new SSH({
+    host: "abomasnow",
+    user: "pi",
+    pass: "r00t",
+  });
+  //'echo "!eVentOpen.py" > file.txt'
+  ssh_options
+    .exec(thecmd, {
+      out: console.log.bind(console),
+    })
+    .start();
+
+  console.log("Executing command move 70" );
+  
+  res.redirect("/MyProp");
+});
+app.post("/moveProp80", (req, res) => {
+  const SSH = require("simple-ssh");
+  const cmd = "eScript";
+  const thecmd = 'echo "!' + cmd + '.py" > file.txt';
+  //  console.log("inside the system " );
+  var ssh_options = new SSH({
+    host: "abomasnow",
+    user: "pi",
+    pass: "r00t",
+  });
+  //'echo "!eVentOpen.py" > file.txt'
+  ssh_options
+    .exec(thecmd, {
+      out: console.log.bind(console),
+    })
+    .start();
+
+  console.log("Executing command move 80" );
+  
+  res.redirect("/MyProp");
+});
+app.post("/moveProp90", (req, res) => {
+  const SSH = require("simple-ssh");
+  const cmd = "eScript";
+  const thecmd = 'echo "!' + cmd + '.py" > file.txt';
+  //  console.log("inside the system " );
+  var ssh_options = new SSH({
+    host: "abomasnow",
+    user: "pi",
+    pass: "r00t",
+  });
+  //'echo "!eVentOpen.py" > file.txt'
+  ssh_options
+    .exec(thecmd, {
+      out: console.log.bind(console),
+    })
+    .start();
+
+  console.log("Executing command move 90" );
+  
+  res.redirect("/MyProp");
+});
+app.post("/moveProp100", (req, res) => {
+  const SSH = require("simple-ssh");
+  const cmd = "eScript";
+  const thecmd = 'echo "!' + cmd + '.py" > file.txt';
+  //  console.log("inside the system " );
+  var ssh_options = new SSH({
+    host: "abomasnow",
+    user: "pi",
+    pass: "r00t",
+  });
+  //'echo "!eVentOpen.py" > file.txt'
+  ssh_options
+    .exec(thecmd, {
+      out: console.log.bind(console),
+    })
+    .start();
+
+  console.log("Executing command move 100" );
+  
+  res.redirect("/MyProp");
+});
+app.post("/moveProp0", (req, res) => {
+  const SSH = require("simple-ssh");
+  const cmd = "eScript";
+  const thecmd = 'echo "!' + cmd + '.py" > file.txt';
+  //  console.log("inside the system " );
+  var ssh_options = new SSH({
+    host: "abomasnow",
+    user: "pi",
+    pass: "r00t",
+  });
+  //'echo "!eVentOpen.py" > file.txt'
+  ssh_options
+    .exec(thecmd, {
+      out: console.log.bind(console),
+    })
+    .start();
+
+  console.log("Executing command move 0" );
+  
+  res.redirect("/MyProp");
+});
 const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, console.log(`Server started on port ${PORT}`));
--- software/src/screens/PropagatorPage.js
+++ software/src/screens/PropagatorPage.js
@@ -83,11 +83,88 @@ export default function MyProp() {
     setStyle2("cont2");
     setStyle("but1");
   };
-  const [val, setVal] = useState([10]);
-  const updateRange = (e, data) => {
+  function moveProp() {
+    axios({
+      method: 'post',
+      url: '/moveProp',
+      data: val
+    });
+  }
+  const [val, setVal] = useState([0]);
+  const updateRange = (e, data) => { //possition
     setVal(data);
+    if(data == 0){
+      axios({
+        method: 'post',
+        url: '/moveProp0',
+        data: val
+      });
+    } else if (data == 10) {
+      axios({
+        method: 'post',
+        url: '/moveProp',
+        data: val
+      });
+    } else if (data == 20) {
+      axios({
+        method: 'post',
+        url: '/moveProp20',
+        data: val
+      });
+    } else if (data == 30) {
+      axios({
+        method: 'post',
+        url: '/moveProp30',
+        data: val
+      });
+    } else if (data == 40) {
+      axios({
+        method: 'post',
+        url: '/moveProp40',
+        data: val
+      });
+    } else if (data == 50) {
+      axios({
+        method: 'post',
+        url: '/moveProp50',
+        data: val
+      });
+    } else if (data == 60) {
+      axios({
+        method: 'post',
+        url: '/moveProp60',
+        data: val
+      });
+    } else if (data == 70) {
+      axios({
+        method: 'post',
+        url: '/moveProp70',
+        data: val
+      });
+    } else if (data == 80) {
+      axios({
+        method: 'post',
+        url: '/moveProp80',
+        data: val
+      });
+    } else if (data == 90) {
+      axios({
+        method: 'post',
+        url: '/moveProp90',
+        data: val
+      });
+    } else if (data == 100) {
+      axios({
+        method: 'post',
+        url: '/moveProp100',
+        data: val
+      });
+    }
+
+    
   };
-  const [val2, setVal2] = useState([10]);
+
+  const [val2, setVal2] = useState([0]);
   const updateRange2 = (e, data) => {
     setVal2(data);
   };
@@ -123,6 +200,7 @@ export default function MyProp() {
     
   });
   }
+  
 
   return (
     <>
@@ -190,18 +268,9 @@ export default function MyProp() {
                   onChange={updateRange}
                 />
               </div>
-              <h2 className="pos">Position: Tilt</h2>
+              
             </div>
-            <div class="slider2">
-              <div class="sliderbar2">
-                <Slider
-                  value={val2}
-                  step={10}
-                  valueLabelDisplay="on"
-                  onChange={updateRange2}
-                />
-              </div>
-            </div>
+            
             <div className="waterCont">
               <div className="watertext">
                 <h2>Watering</h2>
@@ -215,7 +284,7 @@ export default function MyProp() {
           {/*menu*/}
         </div>
       </div>
-      <a
+      {/* <a
         className="App-link"
         href="https://reactjs.org"
         target="_blank"
@@ -225,7 +294,7 @@ export default function MyProp() {
       </a>
       <form action="../../post" method="post" className="form">
         <button type="submit">Connected?</button>
-      </form>
+      </form> */}
       <Footer />
     </>
   );
