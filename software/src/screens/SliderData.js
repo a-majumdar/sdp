dc7d6b38e2c9c39bb4c2639529422f7777ad5062
s1937645 <60388285+s1937645@users.noreply.github.com>
2022-04-02 16:00:46 +0100

    tutorail

--- software/src/screens/SliderData.js
+++ software/src/screens/SliderData.js
@@ -1,5 +1,5 @@
 import cucumPic from "../assets/cucum.jpg";
-<<<<<<< HEAD
+
 import step2 from "../assets/step2.jpg"
 import step1 from "../assets/step1.jpg"
 import step4 from "../assets/step4.jpg"
@@ -41,27 +41,4 @@ export const SliderData = [
 {
   image: step10
 }
-=======
-import Tomato from "../assets/winterplant.jpg";
-import Sunflower from "../assets/sunflower.jpg";
-import cac from "../assets/cactus.jpg";
-import Snowdrop from "../assets/snowdrop.jpg";
-
-export const SliderData = [
-  {
-    image: Sunflower,
-  },
-  {
-    image: Tomato,
-  },
-  {
-    image: cucumPic,
-  },
-  {
-    image: Snowdrop,
-  },
-  {
-    image: cac,
-  },
->>>>>>> a9bdf44de4b3ff2371fafe5d7600cde7cbbc6cf0
 ];
